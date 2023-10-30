99616481bfc391d5052a0aa64761cd3102516413
estii20 <>
2023-10-30 08:43:16 +0000

    Add post to post detail URL so about.html renders

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('post/add/', views.AddPost.as_view(), name='add_post'),
     path('<slug:slug>/edit/', views.EditPost.as_view(),
