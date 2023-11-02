0e82304dc2968396a97866acaeecd390d3902f25
estii20 <>
2023-11-02 11:13:28 +0000

    Add tests to test_models.py and amend database settings for testing

--- blog/test_models.py
+++ blog/test_models.py
@@ -3,8 +3,7 @@ from .models import Post, Comment, AuthorBio, Category
 from django.contrib.auth import get_user_model
 
 
-# Create your tests here.
-USER_MODEL = get_user_model()
+User = get_user_model()
 
 
 class TestModels(TestCase):
@@ -17,7 +16,6 @@ class TestModels(TestCase):
         self.user = User.objects.create(username='nameistest')
         self.user.set_password('passwordistest')
         self.user.save()
-
         self.category = Category.objects.create(
             name=1
         )
@@ -30,13 +28,39 @@ class TestModels(TestCase):
             content='test content',
             excerpt='test excerpt',
             featured_image='test.jpeg',
-            author_bio='This is a test author bio'
+            author_bio='This is a test author bio',
+            status=1
         )
 
         self.comment = Comment.objects.create(
             post=self.post,
-            username=self.user,
-            message='test comment'
+            name=self.user,
+            body='test comment'
         )
 
-        self.read_time = self.content.split(' ') / 250
+    def test_comment_model_str(self):
+        """
+        Test the __str__ method for comment
+        """
+        self.assertEqual(
+            self.comment.__str__(),
+            f'Comment {self.comment.body} by {self.comment.name}'
+        )
+
+    def test_post_and_comment_approved(self):
+        """
+        Test for Comment and post is an approved field
+        """
+        self.assertTrue(self.post.status == 1)
+        self.assertFalse(self.comment.approved)
+
+    def test_absolute_url_str(self):
+        """
+        Test absolute_url for post
+        """
+        self.assertEqual(self.post.get_absolute_url(),
+                         f'/post/{self.post.slug}/')
+
+    def test_post_model__category_str(self):
+        """Test the __str__ method for category post"""
+        self.assertEqual(self.category.__str__(), self.category.name)
