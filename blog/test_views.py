024dbd51b5c8f404a1be727e89eada631ae9c16b
estii20 <>
2023-11-02 11:49:31 +0000

    Add tests to test_views.py

---
+++ blog/test_views.py
@@ -0,0 +1,134 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Post, Comment, Category
+
+
+User = get_user_model()
+
+
+class TestModels(TestCase):
+    """
+    Test blog app models
+    """
+    @classmethod
+    def setUpTestData(self):
+        """Create test data"""
+        self.user = User.objects.create(username='nameistest')
+        self.user.set_password('passwordistest')
+        self.user.save()
+        self.category = Category.objects.create(
+            name=1
+        )
+
+        self.post = Post.objects.create(
+            title='Test Post',
+            slug='test-post',
+            author=self.user,
+            category_id=1,
+            content='test content',
+            excerpt='test excerpt',
+            featured_image='test.jpeg',
+            author_bio='This is a test author bio',
+            status=1
+        )
+
+        self.comment = Comment.objects.create(
+            post=self.post,
+            name=self.user,
+            body='test comment'
+        )
+
+    def test_get_view_post_list(self):
+        """
+        Test to get homepage and template index.html
+        """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html', 'base.html')
+
+    def test_get_post_detail_view(self):
+        """ 
+        Test to get Post Detail page and template post_detail.html
+        """
+        response = self.client.get(
+            reverse('post_detail', args=[self.post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_detail.html', 'base.html')
+
+    def test_get_view_add_post(self):
+        """
+        Test to get Add Post page and template add_post.html
+        """
+        self.client.login(username='nameistest', password='passwordistest')
+        response = self.client.get(reverse('add_post'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_post.html', 'base.html')
+
+    def test_get_view_edit_post(self):
+        """
+        Test to get Edit Post page and template post_detail.html
+        """
+        self.client.login(username='nameistest', password='passwordistest')
+        response = self.client.get(
+            reverse('post_edit', args=[self.post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_edit.html', 'base.html')
+
+    def test_get_view_post_delete(self):
+        """
+        Test to get Post Delete page and template post_delete.html
+        """
+        self.client.login(username='nameistest', password='passwordistest')
+        response = self.client.get(
+            reverse('post_delete', args=[self.post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_delete.html', 'base.html')
+
+    def test_view_post_comment(self):
+        """
+        Test comments for posts
+        """
+        self.client.login(username='nameistest', password='passwordistest')
+        response = self.client.post(
+            reverse('post_detail',
+                    args=[self.post.slug]),
+            data={'body': 'testcomment'})
+        self.assertRedirects(
+            response, reverse('post_detail', args=[self.post.slug]))
+
+    def test_view_post_like(self):
+        """
+        Test for likes for posts
+        """
+        self.client.login(username='nameistest', password='passwordistest')
+        likes = self.post.likes.count()
+        response = self.client.post(
+            reverse('post_like', args=[self.post.slug]))
+        self.assertRedirects(
+            response, reverse('post_detail', args=[self.post.slug]))
+        self.assertEqual(self.post.likes.count(), likes+1)
+        response = self.client.post(
+            reverse('post_like', args=[self.post.slug]))
+        self.assertRedirects(
+            response, reverse('post_detail', args=[self.post.slug]))
+        self.assertEqual(self.post.likes.count(), likes)
+
+    def test_get_view_about(self):
+        """
+        Test to get About page and template about.html
+        """
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_get_view_category(self):
+        """
+        Test to get Category pages and template category.html
+        """
+        response = self.client.get(
+            reverse('category', args=[self.category.name]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'category.html')
