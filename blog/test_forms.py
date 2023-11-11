56a318f56220b104d9e0972566519c59ef7d3b67
estii20 <>
2023-11-11 11:20:05 +0000

    Add tests to test_forms.py

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,5 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Comment, Post
 from .forms import CommentForm, PostForm
+from django_summernote.widgets import SummernoteWidget
+
+
+class FormsTest(TestCase):
+    def setUp(self):
+        """
+        Create a user for testing
+        """
+        self.user = User.objects.create_user(
+            username='nameistest',
+            password='passwordistest'
+        )
 
 
 class TestCommentForm(TestCase):
@@ -25,11 +39,12 @@ class TestCommentForm(TestCase):
 
 class TestPostForm(TestCase):
     """
-    Test fields on post form
+    Tests fields on post form
     """
+
     def test_fields_are_explicit_in_post_metaclass(self):
         """
-        Test fields are equal to post form
+        Tests fields are equal to post form
         """
         form = PostForm()
         self.assertEqual(form.Meta.fields, ['title', 'category', 'author',
@@ -43,3 +58,22 @@ class TestPostForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('category', form.errors.keys())
         self.assertEqual(form.errors['category'][0], 'This field is required.')
+
+    def test_post_form_with_invalid_data(self):
+        """
+        Tests PostForm submission with invalid data
+        """
+        data = {# Missing required fields, intentionally causing validation 
+        #error
+        }
+        form = PostForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_post_form_widget(self):
+        """
+        Tests if SummernoteWidget is applied to content and author_bio fields
+        """
+        form = PostForm()
+        self.assertIsInstance(form.fields['content'].widget, SummernoteWidget)
+        self.assertIsInstance(
+            form.fields['author_bio'].widget, SummernoteWidget)
